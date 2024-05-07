ddf244da6da9c2fb289c72b9f8b59dd98aecc3b1
drewsef-916 <drewm541@gmail.com>
2024-05-07 15:18:16 -0700

    database queries working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,19 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
-const mockUrls = [
-  "https://utfs.io/f/ca70159a-c37e-4b10-a9ec-fe5cf4cf5109-1k819z.jpg",
-  "https://utfs.io/f/74af5f77-7f86-446f-9052-5c8ac0ebd16c-1k81a0.jpg",
-  "https://utfs.io/f/b5105b06-a804-494c-8c46-df8714a7b58b-1k819w.jpg",
-  "https://utfs.io/f/f5ee0648-f4d1-4b64-ad94-6d1bc54da7ab-1k819y.jpg",
-  "https://utfs.io/f/19556780-301c-4735-895d-7e350798d589-1k819x.jpg"
-]
+export default async function HomePage() {
 
-const mockImages = mockUrls.map((url, idx) => ({
-  id: idx + 1,
-  url
-}))
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id)
+  });
 
-export default function HomePage() {
+  console.log(images);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {mockImages.map((image) => (
-          <div key={image.id} className="w-48 p-4">
+        {images.map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48 p-4">
             <img src={image.url} alt="image"/>
           </div>
         ))}
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,18 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { drizzle } from "drizzle-orm/postgres-js";
+import { sql } from "@vercel/postgres";
 import postgres from "postgres";
-
 import { env } from "~/env";
-import * as schema from "./schema";
 
-/**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
- */
-const globalForDb = globalThis as unknown as {
-  conn: postgres.Sql | undefined;
-};
+import * as schema from './schema';
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
-if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+const queryClient = postgres(env.POSTGRES_URL)
 
-export const db = drizzle(conn, { schema });
+export const db = drizzle(queryClient, { schema });
